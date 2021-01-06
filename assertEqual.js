e22dc5f2927264a3a0046cd11448c9652552c65d
Billy <billy_mckinnon@hotmail.com>
2021-01-06 22:51:13 +0000

    assertEqual.js

--- assertEqual.js
+++ assertEqual.js
@@ -1,12 +1,14 @@
-
-const assertEqual = (actual, expected) => {
+const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log(`🛑🛑🛑 Assertion Failed: "${actual}" !== "${expected}"`);
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
-module.exports = assertEqual;
-
-assertEqual;
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("String", "string");
+assertEqual("string", "string");
+assertEqual(42, "42");
+assertEqual(42, 42);
