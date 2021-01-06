f4d722ee363156a4d5fd189eac981267adb7328c
Billy <billy_mckinnon@hotmail.com>
2021-01-06 04:09:08 +0000

    assertArraysEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -21,18 +21,5 @@ const assertArraysEqual = function(array1, array2) {
   }
 };
 
-const without = function(sourceArray, removalArray) {
-  let outputArray = [];
-  for (let i = 0; i < sourceArray.length; i++) {
-    if (!removalArray.includes(sourceArray[i])) {
-      outputArray.push(removalArray[i]);
-    }
-  }
-  return outputArray;
-};
-
-
-const words = ["hello", "world", "lighthouse"];
-without(words, ["lighthouse"]); // no need to capture return value for this test case
-// Make sure the original array was not altered by the without function
-assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([], []);
