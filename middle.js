0e7d69b1b623ec92fb3127ad471c1cd363c88011
Billy <billy_mckinnon@hotmail.com>
2021-01-07 02:20:29 +0000

    middle.js

--- middle.js
+++ middle.js
@@ -1,17 +1,3 @@
-const eqArrays = function(array1, array2) {
-  let isEqual = true;
-  if (array1.length !== array2.length) {
-    isEqual = false;
-  } else {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        isEqual = false;
-      }
-    }
-  }
-  return isEqual;
-};
-
 
 const assertArraysEqual = function(array1, array2) {
   if (eqArrays(array1, array2)) {
@@ -20,8 +6,6 @@ const assertArraysEqual = function(array1, array2) {
     console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
   }
 };
-
-
 
 
 const middle = function(array) {
